48f4f56756b01fdab5dfa66f71ee74274db2f52c
Mhar <mhar0987@live.com>
2017-07-24 01:18:30 +0800

    Initial

---
+++ public/js/pages/forms/basic-form-elements.js
@@ -0,0 +1,33 @@
+$(function () {
+    //Textare auto growth
+    autosize($('textarea.auto-growth'));
+
+    //Datetimepicker plugin
+    $('.datetimepicker').bootstrapMaterialDatePicker({
+        format: 'dddd DD MMMM YYYY - HH:mm',
+        clearButton: true,
+        weekStart: 1
+    });
+    
+    $('.dtpicker').bootstrapMaterialDatePicker({
+        format: 'DD MMMM YYYY',
+        clearButton: true,
+        weekStart: 1,
+        time: false
+    }).on("change" , function(e , date){
+        $(this).focus();
+    });
+
+    $('.datepicker').bootstrapMaterialDatePicker({
+        format: 'dddd DD MMMM YYYY',
+        clearButton: true,
+        weekStart: 1,
+        time: false
+    });
+
+    $('.timepicker').bootstrapMaterialDatePicker({
+        format: 'HH:mm',
+        clearButton: true,
+        date: false
+    });
+});
